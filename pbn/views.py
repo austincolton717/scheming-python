e5038c55d20227e26c35ab543004edd1040c3de5
Austin Colton <austincolton717@gmail.com>
2020-10-26 21:47:07 -0600

    pbn form

--- pbn/views.py
+++ pbn/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render, redirect
+from .forms import ListForm
+from .models import List
 
 
 def pbnlist(request):
-    return render(request, 'pbnfiles/pbnlist.html', {})
+
+    if request.method == 'POST':
+        form = ListForm(request.POST or None)
+
+        if form.is_valid():
+            form.save()
+            all_items = List.objects.all
+            return render(request, 'pbnfiles/pbnlist.html', {'all_itms': all_items})
+
+        else:
+            all_items = List.objects.all
+            return render(request, 'pbnfiles/pbnlist.html', {'all_items': all_items})
